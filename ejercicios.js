cd0921b0e0efa55dba8e8d7647673af1d49d4ac9
Malena Rey <36973939+malerey@users.noreply.github.com>
2021-08-23 19:08:17 -0300

    Create ejercicios.js

---
+++ ejercicios.js
@@ -0,0 +1,39 @@
+
+const frozen = ["Elsa", "Hans", "Olaf", "Ana", "Kristoff"]
+const moana = ["Moana", "Tamatoa", "Maui", "Hei Hei"]
+
+// 1. Hacer una funcion "ordenarAlfabeticamente" que reciba un array y lo retorne ordenado alfabeticamente. 
+
+const ordenarAlfabeticamente = (array) => {}
+
+console.log(ordenarAlfabeticamente(moana))
+console.log(ordenarAlfabeticamente(frozen))
+
+// 2. Hacer una funcion "revertirOrden" que reciba un array y retorne sus elementos en orden inverso. 
+
+const revertirOrden = (array) => {}
+
+console.log(revertirOrden(moana))
+console.log(revertirOrden(frozen))
+
+// 3. Hacer una funcion "incluyeNombre" que reciba un array y un string y retorne true si el array incluye al string, o false en caso contrario. 
+
+const incluyeNombre = (array, nombre) => {}
+
+console.log(incluyeNombre(moana, "Ana"))
+console.log(incluyeNombre(frozen, "Ana"))
+
+// 4. Hacer una funcion "ultimosDos" que reciba un array y retorne un array con los ultimos dos elementos del original. 
+
+const ultimosDos = (array, nombre) => {}
+
+console.log(ultimosDos(moana)) 
+console.log(ultimosDos(frozen))
+
+// 5. Hacer una función "conservarSoloConA" que reciba un array y retorne un nuevo array compuesto solo por los elementos del original que contienen la letra "A". 
+
+const conservarSoloConA = (array, nombre) => {}
+
+console.log(conservarSoloConA(moana)) 
+console.log(conservarSoloConA(frozen))
+
